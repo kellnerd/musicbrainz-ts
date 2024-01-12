c385fb8ea8d29135f44d1e1b12bbb00d2bc2f4bb
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-01-12 19:26:24 +0100

    feat: Create a basic MusicBrainz client which can lookup entities
    Based on my ListenBrainz client, with loose typings only so far.

---
+++ client.ts
@@ -0,0 +1,123 @@
+import { ApiError, isError } from "./error.ts";
+import { assert } from "https://deno.land/std@0.210.0/assert/assert.ts";
+import { delay } from "https://deno.land/std@0.210.0/async/delay.ts";
+import { validate } from "https://deno.land/std@0.210.0/uuid/v4.ts";
+
+/** MusicBrainz API client configuration options. */
+export interface ClientOptions {
+  /**
+   * Root URL of the MusicBrainz API.
+   *
+   * Useful to test with the beta server or a custom server.
+   * @default "https://musicbrainz.org/ws/2/"
+   */
+  apiUrl?: string;
+}
+
+/**
+ * MusicBrainz API client.
+ *
+ * @example
+ * ```ts
+ * const client = new MusicBrainzClient();
+ * ```
+ */
+export class MusicBrainzClient {
+  constructor(options: ClientOptions = {}) {
+    this.apiBaseUrl = options.apiUrl ?? "https://musicbrainz.org/ws/2/";
+
+    this.#headers = {
+      "Accept": "application/json",
+    };
+  }
+
+  /** Performs a lookup request for the given entity. */
+  lookup(entityType: string, mbid: string, inc: string[] = []) {
+    assert(validate(mbid), `${mbid} is not a valid MBID`);
+    return this.get([entityType, mbid].join("/"), { inc: inc.join("+") });
+  }
+
+  /**
+   * Fetches JSON data from the given `GET` endpoint.
+   *
+   * This method should only be directly called for unsupported endpoints.
+   */
+  // deno-lint-ignore no-explicit-any
+  async get(endpoint: string, query?: Query<string | number>): Promise<any> {
+    const endpointUrl = new URL(endpoint, this.apiBaseUrl);
+    if (query) {
+      // Hack to make TS accept query values of type `number`.
+      // https://github.com/microsoft/TypeScript-DOM-lib-generator/issues/1568
+      endpointUrl.search = new URLSearchParams(query as Query).toString();
+    }
+
+    const response = await this.#request(
+      new Request(endpointUrl, {
+        method: "GET",
+        headers: this.#headers,
+      }),
+    );
+
+    const data = await response.json();
+    if (isError(data)) {
+      throw new ApiError(data.error, response.status);
+    } else {
+      return data;
+    }
+  }
+
+  /**
+   * Sends the given JSON data to the given `POST` endpoint.
+   *
+   * This method should only be directly called for unsupported endpoints.
+   */
+  // deno-lint-ignore no-explicit-any
+  async post(endpoint: string, json: any): Promise<any> {
+    const endpointUrl = new URL(endpoint, this.apiBaseUrl);
+    const response = await this.#request(
+      new Request(endpointUrl, {
+        method: "POST",
+        headers: this.#headers,
+        body: JSON.stringify(json),
+      }),
+    );
+
+    const data = await response.json();
+    if (isError(data)) {
+      throw new ApiError(data.error, response.status);
+    } else {
+      return data;
+    }
+  }
+
+  async #request(input: Request | URL): Promise<Response> {
+    await this.#rateLimitDelay;
+
+    const response = await fetch(input);
+
+    /** Number of API usage units remaining in the current time window. */
+    const remainingUnits = response.headers.get("X-RateLimit-Remaining");
+    console.log("X-RateLimit-Remaining", remainingUnits);
+    if (remainingUnits && parseInt(remainingUnits) === 0) {
+      /** Unix time in seconds when the current time window expires. */
+      const rateLimitReset = response.headers.get("X-RateLimit-Reset");
+      if (rateLimitReset) {
+        const rateLimitDelay = parseInt(rateLimitReset) * 1000 - Date.now();
+        if (rateLimitDelay > 0) {
+          console.log("Cool down", rateLimitDelay);
+          this.#rateLimitDelay = delay(rateLimitDelay);
+        }
+      }
+    }
+
+    return response;
+  }
+
+  /** Base URL of the MusicBrainz API endpoints. */
+  apiBaseUrl: string;
+  #headers: HeadersInit;
+  #rateLimitDelay = Promise.resolve();
+}
+
+/** URL query parameters. */
+export type Query<T extends string | number = string> = Record<string, T>;
---
+++ error.ts
@@ -0,0 +1,28 @@
+/** Error which was caused by a MusicBrainz API call. */
+export class ApiError extends Error {
+  /** HTTP status code which was returned by the API. */
+  statusCode: number;
+
+  constructor(message: string, statusCode?: number) {
+    super(message);
+    Object.defineProperty(this, "name", {
+      value: "ApiError",
+      enumerable: false,
+    });
+    this.statusCode = statusCode ?? 500;
+  }
+}
+
+/** Error response which is returned by the MusicBrainz `ws/2` JSON API. */
+export interface ErrorResponse {
+  /** Error message with a description. */
+  error: string;
+  /** Usage help message with link. */
+  help: string;
+}
+
+/** Checks whether the given JSON is an error response. */
+// deno-lint-ignore no-explicit-any
+export function isError(json: any): json is ErrorResponse {
+  return json.error && json.help;
+}
