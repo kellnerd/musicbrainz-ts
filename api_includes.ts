06a9538abe0e50643f3d2c46fb2f4911f1805688
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-03-21 20:07:59 +0100

    chore: Delete unused code from a previous attempt to model includes

--- api_includes.ts
+++ api_includes.ts
@@ -19,7 +19,6 @@ export const relIncludes = entityTypes.map((type) => `${type}-rels`);
 
 export type RelIncludes = `${EntityType}-rels`;
 
-// TODO: Use values below once all include map types are complete
 /** How much of the data about the linked entities should be included. */
 export const subQueryIncludes = [
   // Disc IDs for all media
@@ -31,57 +30,8 @@ export const subQueryIncludes = [
   "artist-credits",
 ] as const;
 
-export const possibleAreaIncludes = [
-  ...miscIncludes,
-] as const;
-
-export type AreaInclude = typeof possibleAreaIncludes[number];
-
-export const possibleArtistIncludes = [
-  "recordings", // TODO
-  "releases", // TODO
-  "release-groups", // TODO
-  "works", // TODO
-  ...miscIncludes,
+const morePossibleArtistIncludes = [
   // Include only those releases where the artist appears on one of the tracks,
   // but not in the artist credit for the release itself.
   "various-artists",
 ] as const;
-
-export type ArtistInclude = typeof possibleArtistIncludes[number];
-
-export const possibleRecordingIncludes = [
-  "artists", // TODO
-  "releases", // TODO
-  "release-groups", // TODO
-  "isrcs", // TODO
-  "artist-credits",
-  ...miscIncludes,
-] as const;
-
-export type RecordingInclude = typeof possibleRecordingIncludes[number];
-
-export const possibleReleaseIncludes = [
-  "artists", // TODO
-  "collections", // TODO
-  "labels",
-  "recordings",
-  "release-groups", // TODO
-  "artist-credits",
-  "discids",
-  ...miscIncludes,
-] as const;
-
-export type ReleaseInclude = typeof possibleReleaseIncludes[number];
-
-export const possibleMediumIncludes = [
-  "discids",
-] as const;
-
-export type MediumInclude = typeof possibleMediumIncludes[number];
-
-export const possibleTrackIncludes = [
-  "artist-credits",
-] as const;
-
-export type TrackInclude = typeof possibleTrackIncludes[number];
