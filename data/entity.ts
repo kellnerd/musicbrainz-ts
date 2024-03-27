690f41f7e4dc1bd83d512bd73ace742ef540d6d8
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-03-27 17:51:04 +0100

    doc: Annotate entity type data module

--- data/entity.ts
+++ data/entity.ts
@@ -1,3 +1,10 @@
+/**
+ * Entity type names and categories.
+ *
+ * @module
+ */
+
+/** MusicBrainz core entity types which can be accessed via API. */
 export const entityTypes = [
   "area",
   "artist",
@@ -15,18 +22,22 @@ export const entityTypes = [
   "work",
 ] as const;
 
+/** MusicBrainz core entity type which can be accessed via API. */
 export type EntityType = typeof entityTypes[number];
 
+/** Plural form of the given entity type. */
 export type EntityPlural<T extends EntityType> =
   // Append a plural "s" to all entity types except "series".
   T extends "series" ? T
     : `${T}s`;
 
+/** Entity type which can be collected. */
 export type CollectableEntityType = Exclude<
   EntityType,
   "collection" | "genre" | "url"
 >;
 
+/** Entity type which can have relationships. */
 export type RelatableEntityType = Exclude<
   EntityType,
   "collection" | "genre"
