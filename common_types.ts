24790e2b59663eac35fa3d6910577bbbf4f8b27e
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-04-16 22:11:05 +0200

    feat: Define type of a release editor seed
    Reusing and adapting the code from kellnerd/harmony,
    which was previously adapted from kellnerd/musicbrainz-scripts.

--- common_types.ts
+++ common_types.ts
@@ -7,6 +7,13 @@
  * @module
  */
 
+/** Date with optional year, month and day. */
+export interface PartialDate {
+  day?: number;
+  month?: number;
+  year?: number;
+}
+
 /** ISO 8601 `YYYY-MM-DD` date, can be partial (`YYYY-MM` or `YYYY`). */
 export type IsoDate =
   | `${number}-${number}-${number}`
@@ -16,10 +23,18 @@ export type IsoDate =
 /** ISO 3166-1 (two letter), 3166-2 or 3166-3 (three letter) code of a country. */
 export type IsoCountryCode = string;
 
-/** ISO 639-3 (three letter) code of a language. */
+/**
+ * [ISO 639-3] (three letter) code of a language.
+ *
+ * [ISO 639-3]: https://en.wikipedia.org/wiki/List_of_ISO_639-3_codes
+ */
 export type IsoLanguageCode = string;
 
-/** ISO 15924 (four letter) code of a script. */
+/**
+ * [ISO 15924] (four letter) code of a script.
+ *
+ * [ISO 15924]: https://en.wikipedia.org/wiki/ISO_15924
+ */
 export type IsoScriptCode = string;
 
 /**
