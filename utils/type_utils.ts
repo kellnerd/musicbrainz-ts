2fce47a0b7e6126877d14dadb7f62b88cdcd1511
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-06-30 16:21:23 +0200

    Recursively turns nullable properties into optional properties

--- utils/type_utils.ts
+++ utils/type_utils.ts
@@ -12,3 +12,21 @@ export type ExpectNot<T extends false> = T;
 export type ToEqual<X, Y> = (<T>() => T extends X ? true : false) extends
   (<T>() => T extends Y ? true : false) ? true
   : false;
+
+/** Recursively turns nullable properties into optional properties. */
+export type OptionalNullable<T> = T extends object ?
+    & {
+      [K in keyof PickNullable<T>]?: OptionalNullable<Exclude<T[K], null>>;
+    }
+    & {
+      [K in keyof PickNotNullable<T>]: OptionalNullable<T[K]>;
+    }
+  : T;
+
+type PickNullable<T> = {
+  [P in keyof T as null extends T[P] ? P : never]: T[P];
+};
+
+type PickNotNullable<T> = {
+  [P in keyof T as null extends T[P] ? never : P]: T[P];
+};
